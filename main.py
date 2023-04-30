5cc336a2abe516d11e75ef6fbdf5432b22e2b145
gurshant <gurshant53@gmail.com>
2023-04-29 20:26:49 -0700

    changed logic for center_input

--- main.py
+++ main.py
@@ -7,30 +7,23 @@ class Poll():
 
         #Setup pins and board
         gpio.setmode(gpio.BCM)
+        # Input high by default
         gpio.setup(4, gpio.IN, pull_up_down=gpio.PUD_UP)
-#        gpio.setup(4, gpio.IN, pull_up_down=gpio.PUD_UP)
- #       gpio.setup(17, gpio.IN, pull_up_down=gpio.PUD_UP)
  
         gpio.setup(5, gpio.OUT)
         gpio.output(5, gpio.HIGH)
 
         #Put pins in variables
         self.center_button = 4
-#         self.right_button = 4
-#         self.left_button = 17
         
         self.first = ''
 
         self.center_press = 1
-#         self.right_press = 0
-#         self.left_press = 0
 
     def reset(self):
         self.first = ''
 
         self.center_press = 1
-#         self.right_press = 0
-#         self.left_press = 0
         
     def poll(self): #Returns first button pressed
         #Variables for press count (one for each button)
@@ -48,55 +41,33 @@ class Poll():
 
     def check(self):
         center_input = gpio.input(self.center_button)
-#         left_input = gpio.input(self.left_button)
-#         right_input = gpio.input(self.right_button)
         print('center_input')
 
         print(center_input)
 
-#         if center_input == True:
-        print('center_press')
-
-        print(self.center_press)
-        if self.center_press == 0:
-            if self.DEBUG:
-                print('ADMIN: Console button 2 has been pressed')
-                
-#                 self.first = 1
-            gpio.output(5, gpio.LOW)
-            time.sleep(1)
-            gpio.output(5, gpio.HIGH)
-
+        if center_input == 0:
+            print('ADMIN: Console button 2 has been pressed')
         else:
-            if self.DEBUG:
-                pass
-        print('ADMIN: Console button 2 has been pressed')
+            print('not pressed')
 
-        self.center_press += 1
 
-        #Right buttons stuff
-#         if right_input == True:
-#             if self.right_press > 0:
-#                 if self.DEBUG:
-#                     print('ADMIN: Console button 3 has been pressed')
-#                 self.first = 2
-#             
-#             else:
-#                 if self.DEBUG:
-#                     pass
-#             self.right_press += 1
-# 
-#         #Left button stuff
-#         if left_input == True:
-#             if self.left_press > 0:
-#                 if self.DEBUG:
-#                     print('ADMIN: Console button 1 has been pressed')
-#                 self.first = 0
-#             
-#             else:
-#                 if self.DEBUG:
-#                     pass
-#             self.left_press += 1
+#         if center_input == True:
+#         print('center_press')
+
+#         print(self.center_press)
+#         if self.center_press == 0:
+#             print('ADMIN: Console button 2 has been pressed')
+                
+# #                 self.first = 1
+#             gpio.output(5, gpio.LOW)
+#             time.sleep(1)
+#             gpio.output(5, gpio.HIGH)
+
+#         else:
+#             pass
+#             print('ADMIN: Console button 2 has been pressed')
+
+#         self.center_press += 1
 
         return self.first
 
