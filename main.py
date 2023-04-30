c3501c7287758a34a88ed694bfee9bc7c4f8cec6
Gurshant <gurshant53@gmail.com>
2023-04-30 14:03:09 -0700

    multi threading setup and now awaiting lights thread

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import RPi.GPIO as gpio
-import time
-import player
-import sounds
+import time, threading
+import player, sounds
 
 class Game():
     def __init__(self):
@@ -10,66 +9,70 @@ class Game():
         self.p2 = player.Player("Player 2",26,6)
 #         self.p3 = player.Player("Player 3",21,7)
 #         self.p4 = player.Player("Player 4",22,8)
-        self.first = ''
+        self.winner = ''
 
 
     def reset(self):
-        self.first = ''
+        self.winner = ''
         
     def game(self): #Returns first button pressed
 
         #Check for button presses
         while True:
-            self.first = self.check()
-            if self.first != '':
+            self.winner = self.check()
+            if self.winner != '':
                 break
-        return self.first
+        return self.winner
 
     def check(self):
         print('running')
         if self.is_button_clicked(self.p1):
-            self.first = '1'
+            self.winner = '1'
         elif self.is_button_clicked(self.p2):
-            self.first = '2'
+            self.winner = '2'
 #         elif self.is_button_clicked(self.p3):
-#             self.first = '3'
+#             self.winner = '3'
 #         elif self.is_button_clicked(self.p4):
-#             self.first = '4'
+#             self.winner = '4'
         else:
             pass
 
-        return self.first
+        return self.winner
 
     def is_button_clicked(self, player):
         if player.input_from_button() == 0:
-            self.sounds.playRightAnswer()
-            self.turn_light_on(player)
-
+#             start threads
+            sounds.play_buzzer_pressed()
+            t2 = threading.Thread( target=self.turn_light_on, args=(player,))
+            t2.start()
+#             wait for thread execution
+            t2.join()
+            print("after play")
             return True
         else:
             return False
         
     def turn_light_on(self, player):
         player.light_on()
-        time.sleep(5.5)
+        time.sleep(3)
         player.light_off()
         time.sleep(.5)
         player.light_on()
-        time.sleep(1)
+        time.sleep(.5)
         player.light_off()
         time.sleep(.5)
         player.light_on()
-        time.sleep(1)
+        time.sleep(.5)
         player.light_off()
         time.sleep(.5)
         player.light_on()
-        time.sleep(1)
+        time.sleep(.5)
         player.light_off()
     
 if __name__ == '__main__':
     test = Game()
     while True:
-        test.first = ''
+        test.winner = ''
         winner = test.game()
         print (winner)
     gpio.cleanup()
--- sounds.py
+++ sounds.py
@@ -1,72 +1,20 @@
-import serial, time, pyaudio, wave, threading, pygame, thread, sys
+import serial, time, wave, threading, pygame, sys
 
-# ser = serial.Serial(sys.argv[1], 9600)
-
-# class AudioThread(threading.Thread):
-#     def __init__(self,fileName):
-#         super(AudioThread, self).__init__()
-#         self.fileName=fileName
-
-#     def run(self):
-#         #define stream chunk   
-#         chunk = 1024  
-
-#         #open a wav format music  
-#         f = wave.open(self.fileName,"rb")  
-
-#         #instantiate PyAudio  
-#         p = pyaudio.PyAudio()  
-
-#       #open stream  
-#         stream = p.open(format = p.get_format_from_width(f.getsampwidth()),
-#             channels = f.getnchannels(),  
-#             rate = f.getframerate(),  
-#             output = True)  
-
-#         #read data  
-#         data = f.readframes(chunk)  
-
-#         #play stream  
-#         while data != '':  
-#             stream.write(data)  
-#             data = f.readframes(chunk) 
-        
-#         #stop stream  
-#         stream.stop_stream()  
-#         stream.close()  
-
-#         #close PyAudio  
-#         p.terminate()  
-
-# class AudioThread2(threading.Thread):
-#     def __init__(self,fileName):
-#         super(AudioThread2, self).__init__()
-#         self.fileName=fileName
-
-#     def run(self):
-#         pygame.mixer.init()
-#         pygame.mixer.music.load(self.fileName)
-#         pygame.mixer.music.play()
-#         while pygame.mixer.music.get_busy() == True:
-#             continue
-
-def playSound(fileName):
+def play_sound(fileName):
     pygame.mixer.init()
     pygame.mixer.music.load(fileName)
     pygame.mixer.music.play()
+    
     while pygame.mixer.music.get_busy() == True:
         continue
 
-def playRightAnswer():
-    # thread = AudioThread2("right_answer.wav")
-    # thread.start()
-    thread.start_new_thread ( playSound, ('right_answer.wav',) )
+def play_right_answer():
+   threading.Thread( target=play_sound, args=('right_answer.wav',) ).start()
+#     playSound('right_answer.wav')
 
-def playBuzzerPressed():
-    # thread = AudioThread2("buzzer_pressed.wav")
-    # thread.start()
-    thread.start_new_thread ( playSound, ('buzzer_pressed.wav',) )
-
-
-
+def play_buzzer_pressed():
+    t1 = threading.Thread( target=play_sound, args=('buzzer_pressed.wav',) )
+    t1.start()
+    t1.join()
+#     playSound('buzzer_pressed.wav')
 
